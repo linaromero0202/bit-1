9a7615eeff15cd38956c74d5406e0669327c638f
Lina Romero <zaesarteaga1902@gmail.com>
2025-05-01 16:53:06 -0500

    cambios

---
+++ lesson.javascript/js/script.js
@@ -0,0 +1,9 @@
+'use strict';
+/*DOM*/
+const $h1 = document.getElementById("titulo")
+$h1.style.color = "blue";
+const $h2 = document.getElementById("subtitulo")
+$h2.style.color = "green";
+
+
+console.log($h2)
